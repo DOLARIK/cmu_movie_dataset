d56f2dc68bbf60e2e1fbaa4ae5f8d991d9bd91c5
Divyanshu Gupta <divyanshu30gupta@gmail.com>
2020-10-06 13:41:49 +0530

    Add project setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+# with open('LICENSE') as f:
+#     license = f.read()
+
+setup(
+    name='cmu_movie_dataset',
+    version='0.1.0',
+    description='For loading CMU Movie Dataset in python',
+    long_description=readme,
+    author='Divyanshu Gupta',
+    author_email='divyanshu30gupta@gmail.com',
+    url='https://github.com/DOLARIK/cmu_movie_dataset',
+    # license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
