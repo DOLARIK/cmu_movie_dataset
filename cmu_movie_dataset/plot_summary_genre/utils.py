d1e5ddfa3f8470f902ccce7bda0ba3dd8f18f3eb
Divyanshu Gupta <divyanshu30gupta@gmail.com>
2020-10-06 14:53:47 +0530

    Add function for reading movie genres

--- cmu_movie_dataset/plot_summary_genre/utils.py
+++ cmu_movie_dataset/plot_summary_genre/utils.py
@@ -1,7 +1,8 @@
 import csv
+import json
 
 def read_plot_summaries(path_to_plot_summaries):
-	"""Reads plot summaries.
+	"""Reads plot_summaries.txt.
 
 	# Arguments:
 		path_to_plot_summaries: str, location of plot_summaries.txt.
@@ -23,9 +24,56 @@ def read_plot_summaries(path_to_plot_summaries):
 
 	return wiki_movie_ids, plot_summaries_dict
 
+def read_movie_genre(path_to_movie_metadata):
+	"""Reads movie genres from movie.metadata.tsv
+
+	# Arguments:
+		path_to_movie_metadata: str, location of movie.metadata.tsv
+
+	# Returns:
+		genre_dict: dict, dictionary that maps wiki movie ids
+			to movie genres
+	"""
+	genre_dict = {}
+	all_genres = set()
+
+	with open(path_to_movie_metadata, 'r') as file:
+		reader = csv.reader(file, delimiter='\t')
+		for row in reader:
+			wiki_movie_id = row[0]
+
+			genre = json.loads(row[-1])
+			genre_values = list(genre.values())
+
+			for genre_value in genre_values:
+				if genre_value not in all_genres:
+					all_genres.add(genre_value)
+
+			genre_dict[wiki_movie_id] = genre_values
+
+
+	return genre_dict, sorted(all_genres)
+			
+
+
 
 if __name__ == "__main__":
-	movie_ids, plot_summaries_dict = read_plot_summaries('../MovieSummaries/plot_summaries.txt')
+	movie_ids, plot_summaries_dict = read_plot_summaries('../../MovieSummaries/plot_summaries.txt')
+
+	genre_dict, all_genres = read_movie_genre('../../MovieSummaries/movie.metadata.tsv')
+
+	# not_found = []
+
+	# for movie_id in movie_ids:
+	# 	try:
+	# 		{
+	# 			'Plot':plot_summaries_dict[movie_id],
+	# 			'Genre':genre_dict[movie_id]
+	# 			}
+	# 	except Exception as e:
+	# 		not_found.append(e)
+
+	# print(not_found, len(not_found))
 
 
 
